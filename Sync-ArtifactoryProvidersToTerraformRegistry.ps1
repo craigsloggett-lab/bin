a5ade79094c157ffb085d2c784e32808e94e1095
Craig Sloggett <craigsloggett@gmail.com>
2025-03-18 11:07:52 -0400

    Updates

--- Sync-ArtifactoryProvidersToTerraformRegistry.ps1
+++ Sync-ArtifactoryProvidersToTerraformRegistry.ps1
@@ -47,16 +47,15 @@ function Sync-ArtifactoryProvidersToTerraformRegistry {
         [ValidateNotNullOrEmpty()]
         [string]$TerraformEnterpriseBearerToken
     )
-    
     begin {
         # Remove all leading and trailing slashes to make path definitions consistent.
-        $ArtifactoryApiUrl = $ArtifactoryApiUrl.TrimEnd('/')
+        $ArtifactoryApiUrl               = $ArtifactoryApiUrl.TrimEnd('/')
         $ArtifactoryItemPropertiesApiUrl = $ArtifactoryItemPropertiesApiUrl.TrimEnd('/')
-        $ArtifactoryRepositoryKey = $ArtifactoryRepositoryKey.TrimStart('/')
-        $ArtifactoryRepositoryKey = $ArtifactoryRepositoryKey.TrimEnd('/')
-        $ArtifactoryRootItemPath = $ArtifactoryRootItemPath.TrimStart('/')
-        $ArtifactoryRootItemPath = $ArtifactoryRootItemPath.TrimEnd('/')
-        $TerraformEnterpriseApiUrl = $TerraformEnterpriseApiUrl.TrimEnd('/')
+        $ArtifactoryRepositoryKey        = $ArtifactoryRepositoryKey.TrimStart('/')
+        $ArtifactoryRepositoryKey        = $ArtifactoryRepositoryKey.TrimEnd('/')
+        $ArtifactoryRootItemPath         = $ArtifactoryRootItemPath.TrimStart('/')
+        $ArtifactoryRootItemPath         = $ArtifactoryRootItemPath.TrimEnd('/')
+        $TerraformEnterpriseApiUrl       = $TerraformEnterpriseApiUrl.TrimEnd('/')
     
         # Create context maps for Artifactory and Terraform Enterprise.
         $ArtifactoryContext = @{
@@ -83,15 +82,31 @@ function Sync-ArtifactoryProvidersToTerraformRegistry {
             [CmdletBinding()]
             param (
                 [Parameter(Mandatory = $true)]
-                [hashtable]$ArtifactoryContext
+                [ValidateNotNullOrEmpty()]
+                [hashtable]$ArtifactoryContext,
+
+                [Parameter(Mandatory = $true)]
+                [string]$CurrentPath
             )
             begin {
                 $headers = $ArtifactoryContext.Headers
-                $uri = "$ArtifactoryContext.ItemPropertiesApiUrl/$ArtifactoryContext.RepositoryKey/$ArtifactoryContext.RootItemPath"
+                $uri     = ("{0}/{1}/{2}/{3}" -f $ArtifactoryContext.ItemPropertiesApiUrl,
+                                                 $ArtifactoryContext.RepositoryKey,
+                                                 $ArtifactoryContext.RootItemPath,
+                                                 $CurrentPath).TrimEnd('/')
             }
             process {
                 # Test if an Artifactory path is a folder, returns True if it is.
                 # ((Invoke-RestMethod -Uri "$uri" -Method GET -Headers $headers -ContentType "application/json").children)
+                Write-Verbose "Querying Artifactory path: $CurrentPath"
+
+                try {
+                    $response = Invoke-RestMethod -Headers $headers -Method GET -Uri $uri
+                }
+                catch {
+                    Write-Error "Failed to query Artifactory with the following error: $_"
+                    return
+                }
                 Write-Verbose "..."
             }
         }
