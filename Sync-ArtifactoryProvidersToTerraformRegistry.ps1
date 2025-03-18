5ac553afbda65d3ce6553fe9a52d6a238d8165cc
Craig Sloggett <craigsloggett@gmail.com>
2025-03-18 09:17:40 -0400

    Refactor to pull out inputs to top-level

--- Sync-ArtifactoryProvidersToTerraformRegistry.ps1
+++ Sync-ArtifactoryProvidersToTerraformRegistry.ps1
@@ -4,7 +4,83 @@
 
 .DESCRIPTION
     TODO: Add a meaningful description.
+
+.EXAMPLE
+    TODO: Add a meaningful example.
 #>
+
+[CmdletBinding()]
+
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ArtifactoryApiUrl,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ArtifactoryItemPropertiesApiUrl,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ArtifactoryAccessToken,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ArtifactoryRepositoryKey,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ArtifactoryRootItemPath,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$TerraformEnterpriseApiUrl,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$TerraformEnterpriseOrganization,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$TerraformEnterpriseBearerToken
+)
+
+begin {
+    # Remove all leading and trailing slashes to make path definitions consistent.
+    $ArtifactoryApiUrl = $ArtifactoryApiUrl.TrimEnd('/')
+    $ArtifactoryItemPropertiesApiUrl = $ArtifactoryItemPropertiesApiUrl.TrimEnd('/')
+    $ArtifactoryRepositoryKey = $ArtifactoryRepositoryKey.TrimStart('/')
+    $ArtifactoryRepositoryKey = $ArtifactoryRepositoryKey.TrimEnd('/')
+    $ArtifactoryRootItemPath = $ArtifactoryRootItemPath.TrimStart('/')
+    $ArtifactoryRootItemPath = $ArtifactoryRootItemPath.TrimEnd('/')
+    $TerraformEnterpriseApiUrl = $TerraformEnterpriseApiUrl.TrimEnd('/')
+
+    # Create context maps for Artifactory and Terraform Enterprise.
+    $ArtifactoryContext = @{
+        ApiUrl               = $ArtifactoryApiUrl 
+        ItemPropertiesApiUrl = $ArtifactoryItemPropertiesApiUrl
+        RepositoryKey        = $ArtifactoryRepositoryKey
+        RootItemPath         = $ArtifactoryRootItemPath
+
+        Headers = @{
+            'X-JFrog-Art-Api' = $ArtifactoryAccessToken
+        }
+    }
+
+    $TerraformEnterpriseContext = @{
+        ApiUrl       = $TerraformEnterpriseApiUrl
+        Organization = $TerraformEnterpriseOrganization
+
+        Headers = @{
+            'Authorization' = "Bearer $TerraformEnterpriseBearerToken"
+        }
+    }
+}
+
+process {
+    Write-Verbose "..."
+    Get-ArtifactoryRepositoryItems -ArtifactoryContext $ArtifactoryContext
+}
 
 # Test if an Artifactory path is a folder, returns True if it is.
 # ((Invoke-RestMethod -Uri "$uri" -Method GET -Headers $headers -ContentType "application/json").children)
@@ -20,77 +96,5 @@ function Get-ArtifactoryRepositoryItems {
         $uri = "$ArtifactoryContext.ItemPropertiesApiUrl/$ArtifactoryContext.RepositoryKey/$ArtifactoryContext.RootItemPath"
     }
     process {
-    }
-}
-
-function Sync-ArtifactoryToTFE {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$ArtifactoryApiUrl,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$ArtifactoryItemPropertiesApiUrl,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$ArtifactoryAccessToken,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$ArtifactoryRepositoryKey,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$ArtifactoryRootItemPath,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$TerraformEnterpriseApiUrl,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$TerraformEnterpriseOrganization,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$TerraformEnterpriseBearerToken
-    )
-    begin {
-        # Remove all leading and trailing slashes to make path definitions consistent.
-        $ArtifactoryApiUrl = $ArtifactoryApiUrl.TrimEnd('/')
-        $ArtifactoryItemPropertiesApiUrl = $ArtifactoryItemPropertiesApiUrl.TrimEnd('/')
-        $ArtifactoryRepositoryKey = $ArtifactoryRepositoryKey.TrimStart('/')
-        $ArtifactoryRepositoryKey = $ArtifactoryRepositoryKey.TrimEnd('/')
-        $ArtifactoryRootItemPath = $ArtifactoryRootItemPath.TrimStart('/')
-        $ArtifactoryRootItemPath = $ArtifactoryRootItemPath.TrimEnd('/')
-        $TerraformEnterpriseApiUrl = $TerraformEnterpriseApiUrl.TrimEnd('/')
-
-        # Create context maps for Artifactory and Terraform Enterprise.
-        $ArtifactoryContext = @{
-            ApiUrl               = $ArtifactoryApiUrl 
-            ItemPropertiesApiUrl = $ArtifactoryItemPropertiesApiUrl
-            RepositoryKey        = $ArtifactoryRepositoryKey
-            RootItemPath         = $ArtifactoryRootItemPath
-
-            Headers = @{
-                'X-JFrog-Art-Api' = $ArtifactoryAccessToken
-            }
-        }
-
-        $TerraformEnterpriseContext = @{
-            ApiUrl       = $TerraformEnterpriseApiUrl
-            Organization = $TerraformEnterpriseOrganization
-
-            Headers = @{
-                'Authorization' = "Bearer $TerraformEnterpriseBearerToken"
-            }
-        }
-    }
-    process {
-        Write-Verbose "..."
-        Get-ArtifactoryRepositoryItems -ArtifactoryContext $ArtifactoryContext
     }
 }
